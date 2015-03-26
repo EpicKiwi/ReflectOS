b6ab78742bb91ffc8ed78a314dab5a460fb5bad3
EpicKiwi <lelycandu76@gmail.com>
2015-03-26 21:42:51 +0100

    Premiere implémentation de Socket.io

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get("/",function(request,response){
 	response.send("Hello world !");
 });
 
-app.listen(80,function(){
+io.on('connection',function(socket){
+	console.log("Connexion socket : "+socket.handshake.address.address);
+});
+
+http.listen(80,function(){
 	console.log("Ecoute de connexion ...");
 });
