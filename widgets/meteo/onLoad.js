b81653bc164d3149cefdf7c7f93d34ee38ec801d
EpicKiwi <lelycandu76@gmail.com>
2015-03-29 20:41:17 +0200

    Transmission de fonctions par Socket

---
+++ widgets/meteo/onLoad.js
@@ -0,0 +1,1 @@
+console.log("Météo chargée");
--- widgets/meteo/widget.js
+++ widgets/meteo/widget.js
@@ -10,7 +10,9 @@ exports.load = function(){
 	var result = {
 		infos: widgetInfos,
 		html: fs.readFileSync(__dirname+"/default.html","UTF-8"),
-		css: fs.readFileSync(__dirname+"/style.css","UTF-8")
+		css: fs.readFileSync(__dirname+"/style.css","UTF-8"),
+		onLoad: "function(){"+fs.readFileSync(__dirname+"/onload.js","UTF-8")+"}",
+		onUpdate: "function(){"+fs.readFileSync(__dirname+"/onupdate.js","UTF-8")+"}"
 	}
 
 	return result;
